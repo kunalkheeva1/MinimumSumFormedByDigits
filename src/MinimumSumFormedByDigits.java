41f9c8e6d6e507fb82a0030f98156fa186d98a70
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-26 22:46:03 -0700

    coded the values for a and b and returned their addition

--- src/MinimumSumFormedByDigits.java
+++ src/MinimumSumFormedByDigits.java
@@ -10,6 +10,16 @@ public class MinimumSumFormedByDigits {
         for(int i=0; i<n; i++){
             pq.add(arr[i]);
         }
+        while(!pq.isEmpty()){
+            if(count%2 == 1){
+                a = a*10 + pq.poll();
+            }
+            else
+            {
+             b = b*10 +pq.poll();
+            }
+            count++;
+        }return a+b;
 
     }
     public static void main(String[] args) {
