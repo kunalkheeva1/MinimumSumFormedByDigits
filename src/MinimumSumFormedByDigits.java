0abfb0eb8009e555e313ecd5dec946a92dcd5f03
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-26 22:48:09 -0700

    added the comments for the clarity

--- src/MinimumSumFormedByDigits.java
+++ src/MinimumSumFormedByDigits.java
@@ -2,14 +2,18 @@ import java.util.PriorityQueue;
 
 public class MinimumSumFormedByDigits {
 
+    //get a minimum heap to always access the smallest element first
     public long minimumSumFormedByDigits(int arr[], int n){
 
         int count =0;
         long a =0, b= 0;
+        //add the elements of given array in minimum heap
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int i=0; i<n; i++){
             pq.add(arr[i]);
         }
+        //first element will make a digit with a, and second with b, then count increases, and finally return
+        //the sum of a and b
         while(!pq.isEmpty()){
             if(count%2 == 1){
                 a = a*10 + pq.poll();
