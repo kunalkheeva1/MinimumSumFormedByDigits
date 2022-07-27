c261cb64949b60ba5d6ac1542ac4b13610a44491
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-26 22:44:01 -0700

    added the elements in the minimum Heap

--- src/MinimumSumFormedByDigits.java
+++ src/MinimumSumFormedByDigits.java
@@ -1,5 +1,18 @@
+import java.util.PriorityQueue;
+
 public class MinimumSumFormedByDigits {
+
+    public long minimumSumFormedByDigits(int arr[], int n){
+
+        int count =0;
+        long a =0, b= 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<n; i++){
+            pq.add(arr[i]);
+        }
+
+    }
     public static void main(String[] args) {
-        
+
     }
 }
